9d7ba1956b89b6531b5e9095ddcda8a1123f56ad
Shubham <sk5620526@gmail.com>
2024-09-18 21:48:33 +0530

    Github

---
+++ Backend/database/dbConnection.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const dbConnection = () =>{
+    mongoose.connect(process.env.MONGO_URI, {
+        dbName: "anand"
+    }).then(() =>{
+        console.log('connected to database');
+    }).catch((err) =>{
+        console.log(`some error occured while connecting to database: ${err}`);
+    });
+};
